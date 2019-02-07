51cc76a8158e5bcf69eaeacfd4b0d1fc24efd0ce
AzureAD\RobinSchroeder <robin@msctek.com>
2019-02-06 22:20:35 -0600

    Initial checkin of simple solution, about page is complete

---
+++ simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using QuikRide.Models;
+
+namespace QuikRide.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ simpleSoln/QuikRide/QuikRide/QuikRide/Views/AboutPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QuikRide.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Facebook_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://facebook.com"));
+        }
+
+        private void PhoneNumber_Tapped(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("11234567890");
+            }
+            catch (FeatureNotSupportedException)
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "Sorry, the phone dialer is not supported on this device.", "OK");
+            }
+            catch (Exception ex)
+            {
+                Application.Current.MainPage.DisplayAlert("Error", ex.Message, "OK");
+            }
+        }
+
+        private void Slack_Tapped(object sender, EventArgs e)
+        {
+            Application.Current.MainPage.DisplayAlert(
+                        "Wait!",
+                        "You must be a member to join our Slack Channel!",
+                        "OK");
+        }
+
+        private void Twitter_Tapped(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://twitter.com"));
+        }
+    }
+}
