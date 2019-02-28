0ce058428475712bb099dfadc033c5e2d4ca81f9
robintschroeder <robintschroeder@gmail.com>
2019-02-27 22:20:45 -0600

    ready for class feb 28

--- simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/MyReservationRequestsViewModel.cs
+++ simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/MyReservationRequestsViewModel.cs
@@ -1,23 +1,18 @@
-﻿using GalaSoft.MvvmLight.Command;
-using QuikRide.Interfaces;
-using QuikRide.ModelsObj;
-using System;
-using System.Collections.ObjectModel;
+﻿using QuikRide.Interfaces;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
 namespace QuikRide.ViewModels
 {
     public class MyReservationRequestsViewModel : CustomViewModelBase
     {
-        private ObservableCollection<ReservationRequest> _myRequests;
+        //private ObservableCollection<ReservationRequest> _myRequests;
 
         public MyReservationRequestsViewModel(INavigationService navService) : base(navService)
         {
-            MyRequests = new ObservableCollection<ReservationRequest>();
+            //MyRequests = new ObservableCollection<ReservationRequest>();
         }
 
-        public RelayCommand<Guid> CancelCommand
+        /*public RelayCommand<Guid> CancelCommand
         {
             get
             {
@@ -27,22 +22,22 @@ namespace QuikRide.ViewModels
                     await Application.Current.MainPage.DisplayAlert("TODO", $"Write code to cancel this reservation: {id}!", "OK");
                 });
             }
-        }
+        }*/
 
-        public ObservableCollection<ReservationRequest> MyRequests
+        /*public ObservableCollection<ReservationRequest> MyRequests
         {
             get { return _myRequests; }
             set { Set(() => MyRequests, ref _myRequests, value); }
-        }
+        }*/
 
         public override async Task Init()
         {
             //we usually do async calls here to get data here, but sample data is call synchronously
-            MyRequests = new ObservableCollection<ReservationRequest>()
+            /*MyRequests = new ObservableCollection<ReservationRequest>()
             {
                 SampleData.SampleReservationRequest.Sample1,
                 SampleData.SampleReservationRequest.Sample2
-            };
+            };*/
         }
     }
 }
