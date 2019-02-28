0f18ae2351646d4182d4de8022b95924caeda6c3
robintschroeder <robintschroeder@gmail.com>
2019-02-27 20:47:26 -0600

    Restoring page from class last week, trying to get ios icons showing up

--- simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/AboutViewModelMVVM.cs
+++ simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/AboutViewModelMVVM.cs
@@ -1,7 +1,7 @@
 ﻿using GalaSoft.MvvmLight.Command;
 using System;
 
-//using System.Collections.Generic;
+using System.Collections.Generic;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -14,8 +14,8 @@ namespace QuikRide.ViewModels
 
         public AboutViewModelMVVM()
         {
-            //AboutText1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat massa vitae turpis vestibulum, id ultrices ante suscipit. Morbi feugiat massa ut eros imperdiet luctus. Sed dapibus, magna sed varius accumsan, sem nibh elementum massa, vitae sodales libero erat eget purus. Maecenas nisl nibh, commodo quis tortor eu, semper facilisis erat. Maecenas at auctor risus, at pulvinar sem. Vestibulum elementum, purus venenatis laoreet ornare, magna metus condimentum quam, at varius lorem elit nec nisl. Donec nec rhoncus ipsum, a imperdiet erat.";
-            //AboutText2 = "Morbi a neque id diam pellentesque dictum vel eu justo.Fusce quis nisl sit amet turpis euismod pulvinar. Aliquam erat volutpat.Interdum et malesuada fames ac ante ipsum primis in faucibus.Suspendisse id nisi varius velit posuere egestas sit amet in felis.Sed facilisis nulla libero, ornare pharetra sapien tincidunt at. Pellentesque pellentesque leo id suscipit consectetur. Aliquam sed enim sit amet nulla facilisis semper. Fusce porttitor tristique ex, non egestas dolor euismod et. Sed arcu arcu, venenatis ac congue posuere, laoreet et urna. Nulla tristique ante sed enim laoreet, sed blandit nulla ullamcorper.Nunc elementum tincidunt turpis. Pellentesque laoreet ipsum quis venenatis maximus. Pellentesque et ligula sed sem porttitor scelerisque.";
+            AboutText1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur volutpat massa vitae turpis vestibulum, id ultrices ante suscipit. Morbi feugiat massa ut eros imperdiet luctus. Sed dapibus, magna sed varius accumsan, sem nibh elementum massa, vitae sodales libero erat eget purus. Maecenas nisl nibh, commodo quis tortor eu, semper facilisis erat. Maecenas at auctor risus, at pulvinar sem. Vestibulum elementum, purus venenatis laoreet ornare, magna metus condimentum quam, at varius lorem elit nec nisl. Donec nec rhoncus ipsum, a imperdiet erat.";
+            AboutText2 = "Morbi a neque id diam pellentesque dictum vel eu justo.Fusce quis nisl sit amet turpis euismod pulvinar. Aliquam erat volutpat.Interdum et malesuada fames ac ante ipsum primis in faucibus.Suspendisse id nisi varius velit posuere egestas sit amet in felis.Sed facilisis nulla libero, ornare pharetra sapien tincidunt at. Pellentesque pellentesque leo id suscipit consectetur. Aliquam sed enim sit amet nulla facilisis semper. Fusce porttitor tristique ex, non egestas dolor euismod et. Sed arcu arcu, venenatis ac congue posuere, laoreet et urna. Nulla tristique ante sed enim laoreet, sed blandit nulla ullamcorper.Nunc elementum tincidunt turpis. Pellentesque laoreet ipsum quis venenatis maximus. Pellentesque et ligula sed sem porttitor scelerisque.";
         }
 
         public string AboutText1
@@ -36,7 +36,7 @@ namespace QuikRide.ViewModels
             {
                 return new RelayCommand(async () =>
                 {
-                    //await Helpers.Helpers.SendEmail("Question about QuikTrip", "I was wondering...", new List<string>() { "info@quikride.com" });
+                    await Helpers.Helpers.SendEmail("Question about QuikTrip", "I was wondering...", new List<string>() { "info@quikride.com" });
                 });
             }
         }
@@ -47,7 +47,7 @@ namespace QuikRide.ViewModels
             {
                 return new RelayCommand(() =>
                 {
-                    //Device.OpenUri(new Uri("https://facebook.com"));
+                    Device.OpenUri(new Uri("https://facebook.com"));
                 });
             }
         }
@@ -60,7 +60,7 @@ namespace QuikRide.ViewModels
                 {
                     try
                     {
-                        //PhoneDialer.Open("16303449385");
+                        PhoneDialer.Open("16303449385");
                     }
                     catch (FeatureNotSupportedException)
                     {
@@ -88,7 +88,7 @@ namespace QuikRide.ViewModels
             {
                 return new RelayCommand(() =>
                 {
-                    //Application.Current.MainPage.DisplayAlert("Wait!", "You must be a member to join our Slack Channel!", "OK");
+                    Application.Current.MainPage.DisplayAlert("Wait!", "You must be a member to join our Slack Channel!", "OK");
                 });
             }
         }
@@ -99,7 +99,7 @@ namespace QuikRide.ViewModels
             {
                 return new RelayCommand(() =>
                 {
-                    //Device.OpenUri(new Uri("https://twitter.com"));
+                    Device.OpenUri(new Uri("https://twitter.com"));
                 });
             }
         }
