e988133fabebef2762f319cc688015d2b0790792
robintschroeder <robintschroeder@gmail.com>
2019-02-28 17:12:52 -0600

    stuff from class 2/28

--- simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/MyReservationRequestsViewModel.cs
+++ simpleSoln/QuikRide/QuikRide/QuikRide/ViewModels/MyReservationRequestsViewModel.cs
@@ -1,43 +1,59 @@
-﻿using QuikRide.Interfaces;
+﻿using GalaSoft.MvvmLight.Command;
+using QuikRide.Interfaces;
+using QuikRide.ModelsObj;
+using System;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace QuikRide.ViewModels
 {
     public class MyReservationRequestsViewModel : CustomViewModelBase
     {
-        //private ObservableCollection<ReservationRequest> _myRequests;
+        private ObservableCollection<ReservationRequest> _myRequests;
 
         public MyReservationRequestsViewModel(INavigationService navService) : base(navService)
         {
-            //MyRequests = new ObservableCollection<ReservationRequest>();
+            MyRequests = new ObservableCollection<ReservationRequest>();
         }
 
-        /*public RelayCommand<Guid> CancelCommand
+        public RelayCommand<Guid> CancelCommand
         {
             get
             {
                 return new RelayCommand<Guid>(async (id) =>
                 {
-                    //TODO: cancel
-                    await Application.Current.MainPage.DisplayAlert("TODO", $"Write code to cancel this reservation: {id}!", "OK");
+                    //TODO: rts cancel
+                    await Application.Current.MainPage.DisplayAlert("TODO",
+                        $"Write code to cancel this reservation: {id}!", "OK");
                 });
             }
-        }*/
+        }
 
-        /*public ObservableCollection<ReservationRequest> MyRequests
+        public RelayCommand MapCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                });
+            }
+        }
+
+        public ObservableCollection<ReservationRequest> MyRequests
         {
             get { return _myRequests; }
             set { Set(() => MyRequests, ref _myRequests, value); }
-        }*/
+        }
 
         public override async Task Init()
         {
             //we usually do async calls here to get data here, but sample data is call synchronously
-            /*MyRequests = new ObservableCollection<ReservationRequest>()
+            MyRequests = new ObservableCollection<ReservationRequest>()
             {
                 SampleData.SampleReservationRequest.Sample1,
                 SampleData.SampleReservationRequest.Sample2
-            };*/
+            };
         }
     }
 }
