ba8f159bc4375805dd4db08b250d47b1e67193b2
Paul <Paul@msctek.com>
2019-05-11 11:39:18 -0500

    Removing Xamarin client-side CodeGenHero-generated files from the StarterSolution.

--- StarterSolution/CGH.QuikRide.Constants/Enums.cs
+++ StarterSolution/CGH.QuikRide.Constants/Enums.cs
@@ -2,13 +2,7 @@
 
 namespace CodeGenHero.ResourceScheduler.Constants
 {
-    public static class Enums
-    {
-        //public enum BingoInstanceStatusType
-        //{
-        //    New = 1,
-        //    Active = 2,
-        //    Complete = 4
-        //}
-    }
+	public static class Enums
+	{
+	}
 }
